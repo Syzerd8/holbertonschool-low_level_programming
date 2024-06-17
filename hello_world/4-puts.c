16b51fbc056d0acedc7829627db597188362fb55
Syzerd8 <hugo1611@gmail.com>
2024-06-17 15:27:44 +0200

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-gcc -o 4-puts.c ; ltrace puts ""Programming is like building a multilingual puzzle\n""c
+#include
+int main(void)
+{
+puts ("Programming is like building a multilingual puzzle\n");
+return 0;
+}
