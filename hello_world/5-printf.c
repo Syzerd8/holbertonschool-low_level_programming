a54d19783952cffe80ab1174c267e0e65ceb58e3
Syzerd8 <hugo1611@gmail.com>
2024-06-18 10:25:07 +0200

    2eme C

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - 2eme tache C
+ * Return: fonction printf, puts interdit et retour 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
