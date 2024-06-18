f8e4747281ba13605aaa8872d9f81deaf98f8212
Syzerd8 <hugo1611@gmail.com>
2024-06-18 11:03:45 +0200

    3eme tache C

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - 3eme tache C
+ *size of various typesonj the computer
+ *Return: always 0 u know what i mean
+ */
+int main(void)
+{
+	printf(\nsize of the integer data type is % d",sizeof(int));
+	return (0);
+}
