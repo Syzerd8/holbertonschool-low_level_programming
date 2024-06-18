fb261291478950e6530ac3730a690d0adb426a23
Syzerd8 <hugo1611@gmail.com>
2024-06-18 09:56:43 +0200

    first C task youhou

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include
+#include <stdio.h>
+
+/**
+ * main - premiere tache C
+ * Return: 0 = exo reussi
+ */
 int main(void)
 {
-puts ("Programming is like building a multilingual puzzle\n");
-return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
