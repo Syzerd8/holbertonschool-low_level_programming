1dccba231d35f52a3a2adec31a2e2faf6469d295
Syzerd8 <hugo1611@gmail.com>
2024-06-19 14:54:19 +0200

    cc

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - lower case, upper case
+ * putchar cuz no printf, puts
+ * Return: lets get it
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
