8536d021eed8a104ad6f0ffdc392dfae0ff90c54
Syzerd8 <hugo1611@gmail.com>
2024-06-19 12:16:00 +0200

    alphabet lowercase

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
- /**
+/**
  * main - lower case
  * putchar cuz no printf, puts
- * Return: one day i will
+ * Return: 0 one day i will
  */
-	int main(void)
+
+int main(void)
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-{
+	{
 		putchar(ch);
-}
-		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }
-
