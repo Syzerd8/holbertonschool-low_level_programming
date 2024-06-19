859e0e62265797a4a6b0af6593d8a412a6935a63
Syzerd8 <hugo1611@gmail.com>
2024-06-19 17:05:12 +0200

    number of base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - droite ou gauche ?
+ * Return: all numbers of base 10 starting from 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
