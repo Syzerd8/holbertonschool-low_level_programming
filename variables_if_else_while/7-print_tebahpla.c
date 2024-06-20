c753e40c7c101c421d8ccb1de8209b0afd457909
Syzerd8 <hugo1611@gmail.com>
2024-06-20 10:14:59 +0200

    reverse abc

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - lower case in reverse
+ * putchar cuz no printf, puts
+ * Return: first try ez ?
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
