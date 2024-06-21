090d6ac8833ca3b725af5c5d1a4cc98458674a4c
Syzerd8 <hugo1611@gmail.com>
2024-06-21 14:16:51 +0200

    alpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - 10 times abc
+ * @c: isnogood
+ * Return: Always 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
