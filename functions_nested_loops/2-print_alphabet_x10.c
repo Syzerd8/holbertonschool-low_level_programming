879ad86f706bf4bfd2f99de092c73f0f8340d9e0
Syzerd8 <hugo1611@gmail.com>
2024-06-21 13:12:08 +0200

    10 times abc

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - 10 times abc
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i;
+
+	for (i = 0; i < 10; i++)
+		for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
