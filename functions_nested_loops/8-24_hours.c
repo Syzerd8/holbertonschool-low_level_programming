b3f4a8cd6b82375aa3810edc806fbfc36e5ebd3d
Syzerd8 <hugo1611@gmail.com>
2024-06-23 19:14:45 +0200

    24

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,17 +8,17 @@
 
 void jack_bauer(void)
 {
-	int m, h;
+	int h, m;
 
 	for (m = 0; h <= 23; h++)
 	{
 		for (m = 0; m <= 59; m++)
 		{
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
-			_putchar(':');
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
 		}
 	}
