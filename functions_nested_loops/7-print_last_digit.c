aa77749fe2e4813076d546610b095fa279c7a7b8
Syzerd8 <hugo1611@gmail.com>
2024-06-23 17:52:14 +0200

    pls print that last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - 10 times abc
+ * @z: isnogood
+ * Return: Always 0
+ */
+
+int print_last_digit(int n)
+{
+	int ld;
+
+	if (n < 0)
+	{
+		ld = -1 * (n % 10);
+		_putchar(ld + '0');
+		return (ld);
+		}
+	else
+		{
+		ld = n % 10;
+		_putchar(ld + '0');
+		return (ld);
+		}
+}
