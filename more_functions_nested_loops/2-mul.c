3e18c560435ffb9adcffe080ea186d70de5c9741
Syzerd8 <hugo1611@gmail.com>
2024-06-25 09:01:22 +0200

    multipl

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplier 2 entiers
+ * @a: 1
+ * @b: 2
+ * Return: ab
+ */
+
+int mul(int a, int b)
+{
+	int z;
+
+		z = a * b;
+		return (z);
+}
