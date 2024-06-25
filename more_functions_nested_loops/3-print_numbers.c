fc41f73542636863a58c5edbce315513e1290c38
Syzerd8 <hugo1611@gmail.com>
2024-06-25 09:29:31 +0200

    0to9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - 0 to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int z;
+
+	for (z = '0'; z <= '9'; z++)
+		{
+		_putchar(z + '0');
+		}
+
+	_putchar('\n');
+}
