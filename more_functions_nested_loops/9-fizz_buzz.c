a9f8e40b426e022485888ff46bbe8030a0df29d7
Syzerd8 <hugo1611@gmail.com>
2024-06-25 15:15:00 +0200

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - multiple de 3
+ * multiple de 5
+ * multiple de 15
+ * Return: 0
+*/
+
+int main(void)
+{
+	int z;
+
+	for (z = 1; z <= 100; z++)
+	{
+		if (z % 3 == 0 && z % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (z % 5 == 0 && z % 3 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (z % 5 == 0 && z % 3 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (z == 1)
+		{
+			printf("%d", z);
+		}
+		else
+		{
+			printf("%d", z);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
