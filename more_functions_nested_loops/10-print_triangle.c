2f73bc5e1b9ea0aabf615442a30fc363dd6ccdfd
Syzerd8 <hugo1611@gmail.com>
2024-06-25 17:03:54 +0200

    pyramide

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,16 +10,23 @@ void print_triangle(int size)
 {
 	int x, y, z;
 
-	for (x = 0; x < size; x++)
+	if (size <= 0)
 	{
-		for (y = 0; y < size - 1 - x; y++)
-		{
-			_putchar(' ');
-		}
-		for (z = size - x - 1; z < size; z++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size - 1 - x; y++)
+			{
+				_putchar(' ');
+			}
+			for (z = size - x - 1; z < size; z++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
