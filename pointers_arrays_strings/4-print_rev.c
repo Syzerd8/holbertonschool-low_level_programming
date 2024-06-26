24af832a44a01b05a32eae17b78be051ce5cfbf1
Syzerd8 <hugo1611@gmail.com>
2024-06-26 20:54:07 +0200

    Reverse string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - print string
+ * @s: *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+	for (c = c - 1; s[c] != '\0'; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
