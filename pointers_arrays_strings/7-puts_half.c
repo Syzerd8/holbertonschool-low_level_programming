cff5db633cccbc92af907eee2459c6bdae00687e
Syzerd8 <hugo1611@gmail.com>
2024-06-27 16:34:37 +0200

    Half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - half of string
+ * @str: *
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int i, a;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		a = length / 2;
+	}
+	else
+	{
+		a = (length - 1) / 2 + 1;
+	}
+	for (i = a; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
