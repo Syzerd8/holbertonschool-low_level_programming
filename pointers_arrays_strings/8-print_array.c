ee5a7a1db4cfe4c9cf103bec9dafa8d11918f2d2
Syzerd8 <hugo1611@gmail.com>
2024-06-28 10:15:11 +0200

    printf elements of an array integers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_array - marche stp
+ * @a: print n elements
+ * @n: array integers
+ * Return: 0
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		printf(", ");
+	}
+	printf("\n");
+}
