3581ee4540af33d50ae45a7a6762094575a7e691
Syzerd8 <hugo1611@gmail.com>
2024-07-01 16:15:02 +0200

    encoding a string

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - change lowercase of a string to uppercase
+ * @ptr: look up !
+ * Return: 0
+ */
+char *leet(char *ptr)
+{
+	char *z = ptr;
+	char a[] = {'a', 'e', 'o', 't', 'l'};
+	char n[] = {4, 3, 0, 7, 1};
+	int i = 0;
+
+	while (*ptr)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*ptr == a[i] || *ptr == a[i] - 32)
+				*ptr = n[i] + '0';
+		}
+		ptr++;
+	}
+	return(z);
+}
